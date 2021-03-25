5c3421542c91dbeb97e4b3c35841e445e9743d99
James Bell <james989@outlook.com>
2021-03-25 11:11:17 -0500

    Add project files.

---
+++ File Creator/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace File_Creator
+{
+    partial class frmFileCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFileCreator));
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cboExtensions = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.extensionsTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(119, 14);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(175, 22);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(16, 19);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(75, 17);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "File Name:";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(356, 74);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(13, 217);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(140, 30);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "&Generate File";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(230, 217);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Clear Fields";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(16, 143);
+            this.lblPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(67, 17);
+            this.lblPath.TabIndex = 7;
+            this.lblPath.Text = "File Path:";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(119, 138);
+            this.txtFilePath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(329, 22);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(348, 217);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 30);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cboExtensions
+            // 
+            this.cboExtensions.DropDownHeight = 105;
+            this.cboExtensions.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cboExtensions.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cboExtensions.FormattingEnabled = true;
+            this.cboExtensions.IntegralHeight = false;
+            this.cboExtensions.Items.AddRange(new object[] {
+            "c",
+            "class",
+            "cs",
+            "css ",
+            "html",
+            "java",
+            "js ",
+            "php"});
+            this.cboExtensions.Location = new System.Drawing.Point(119, 74);
+            this.cboExtensions.Name = "cboExtensions";
+            this.cboExtensions.Size = new System.Drawing.Size(175, 24);
+            this.cboExtensions.Sorted = true;
+            this.cboExtensions.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Extension:";
+            // 
+            // frmFileCreator
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(464, 260);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboExtensions);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.txtFileName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmFileCreator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cboExtensions;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolTip extensionsTooltip;
+    }
+}
+
---
+++ File Creator/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Creator
+{
+    public partial class frmFileCreator : Form
+    {
+        public frmFileCreator()
+        {
+            InitializeComponent();
+            extensionsTooltip.SetToolTip(cboExtensions, "Select an option or type a custom extension");
+
+            //try to set path to current user's desktop
+            try
+            {
+                txtFilePath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+
+            catch (Exception)
+            {
+                txtFilePath.Text = "C:/"; //seems like a reasonable default value
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //check if input fields are empty
+            if (String.IsNullOrEmpty(txtFileName.Text.Trim()) || String.IsNullOrEmpty(txtFilePath.Text.Trim()) || String.IsNullOrEmpty(cboExtensions.Text.Trim()))
+            {
+                MessageBox.Show("All fields are required", "Error");
+            }
+
+            else
+            {
+                try
+                {
+                    //TODO check if file w/same name & extension already exists in directory, warn user
+
+                    //create file 
+                    String path = txtFilePath.Text.Trim() + "/" + txtFileName.Text.Trim() + "." + cboExtensions.Text.Trim();
+                    FileStream fs = File.Create(path);
+                    fs.Close();
+                    
+                    MessageBox.Show(txtFileName.Text.Trim() + "." + cboExtensions.Text.Trim() + " created in " + txtFilePath.Text.Trim(), "Success");
+                }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Unable to create file. Verify file extension and path are valid.", "Error");
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFileName.Text = "";
+            txtFilePath.Text = "";
+            cboExtensions.Text = "";
+        }
+    }
+}
